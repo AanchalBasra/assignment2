c987ac6451bbacd3d6c5380bf0bfd1479bb9c9e6
Aanchal Basra <abasra3@myseneca.ca>
2023-06-02 19:41:12 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+// Serve static files from the "public" directory
+app.use(express.static('public'));
+
+// Redirect "/" to "/about"
+app.get('/', (req, res) => {
+  res.redirect('/about');
+});
+
+// Return the about.html file from the views folder
+app.get('/about', (req, res) => {
+  res.sendFile(__dirname + '/views/about.html');
+});
+
+// Start the server
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Express http server listening on port ${port}`);
+});
